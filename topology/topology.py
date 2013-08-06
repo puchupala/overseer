a2edae5feaf552e1b84483d2dde9611de373d14b
Pongsakorn U-chupala <knightbaron@gmail.com>
2013-08-06 15:58:56 +0700

    Refactor openflow_topology._handle_openflow_discovery_LinkEvent()

--- topology/topology.py
+++ topology/topology.py
@@ -34,36 +34,67 @@ class Topology (EventMixin):
     self.graph = nx.Graph()  # Use dpid as node
 
   def _handle_openflow_discovery_LinkEvent (self, event):
+    # added/removed may be duplicated as links are two-way
+    if event.added:
+      self.__handle_openflow_discovery_LinkUp(event)
+    else:
+      self.__handle_openflow_discovery_LinkDown(event)
+
+  def __handle_openflow_discovery_LinkUp (self, event):
+    """
+    Handle for LinkUp psuedo-event
+
+    method name was deliberatly spell like this to keep  automatic
+    event handling with self.listen_to_dependencies(self) happy.
+    openflow_discovery don't actually have LinkUp but we
+    want to write it this way to keep the code tidy.
+    """
     link = event.link
     # Note that dpid might be Int or Long or perhaps something else entirely!
     # We may need to care about their types if problems arise
     dpid1 = link.dpid1
     dpid2 = link.dpid2
 
-    # added/removed may be duplicated as links are two-way
-    if event.added:
-      self.log.info("Link Up: %s - %s" % (dpid1, dpid2))
-      self.graph.add_edge(dpid1, dpid2, portByDpid={
-        dpid1: link.port1,
-        dpid2: link.port2,
-      })
-      self.raiseEvent(events.LinkUp, dpid1, dpid2)
-    else:
-      self.log.info("Link Down: %s - %s" % (dpid1, dpid2))
-      try:
-        self.graph.remove_edge(dpid1, dpid1)
-      except nx.NetworkXError as e:
-        # Edge was already removed
-        self.log.info(e.message)
+    if self.graph.has_edge(dpid1, dpid2):
+      return  # Duplicated edge
 
-      # Clear the entire flow table of all switches!
-      clear = of.ofp_flow_mod(command=of.OFPFC_DELETE)
-      for switch in self.graph.nodes():
-        self.graph.node[switch]["connection"].send(clear)
+    self.log.info("Link Up: %s - %s" % (dpid1, dpid2))
+    self.graph.add_edge(dpid1, dpid2, portByDpid={
+      dpid1: link.port1,
+      dpid2: link.port2,
+    })
+    self.raiseEvent(events.LinkUp, dpid1, dpid2)
 
-      # TODO: Remove isolated node if any
+  def __handle_openflow_discovery_LinkDown (self, event):
+    """
+    Handle for LinkDown psuedo-event
 
-      self.raiseEvent(events.LinkDown, dpid1, dpid2)
+    method name was deliberatly spell like this to keep  automatic
+    event handling with self.listen_to_dependencies(self) happy.
+    openflow_discovery don't actually have LinkDown but we
+    want to write it this way to keep the code tidy.
+    """
+    link = event.link
+    # Note that dpid might be Int or Long or perhaps something else entirely!
+    # We may need to care about their types if problems arise
+    dpid1 = link.dpid1
+    dpid2 = link.dpid2
+
+    self.log.info("Link Down: %s - %s" % (dpid1, dpid2))
+    try:
+      self.graph.remove_edge(dpid1, dpid1)
+    except nx.NetworkXError as e:
+      # Edge was already removed
+      self.log.info(e.message)
+      return
+
+    # Clear the entire flow table of all switches!
+    clear = of.ofp_flow_mod(command=of.OFPFC_DELETE)
+    for switch in self.graph.nodes():
+      self.graph.node[switch]["connection"].send(clear)
+
+    # TODO: Remove isolated node if any
+    self.raiseEvent(events.LinkDown, dpid1, dpid2)
 
   def _handle_openflow_ConnectionUp (self, event):
     self.log.info("Connection Up: %s" % event.dpid)
